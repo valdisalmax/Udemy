9d21ccfb8923650f01005fefd81bd356b6dcdbfd
valdisalmax <b.lodder@bl-ict.nl>
2019-01-19 18:22:28 +0100

    Operators part 2

--- Java Masterclass/Operators/src/com/company/Main.java
+++ Java Masterclass/Operators/src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    int result = 1 + 2;
+        int result = 1 + 2;
         System.out.println("1 + 2 = " + result);
 
         int previousResult = result;
@@ -58,8 +58,44 @@ public class Main {
             System.out.println("You got the high score!");
 
         int secondTopScore = 60;
-        if (topScore > secondTopScore && topScore < 100)
+        if ((topScore > secondTopScore) && (topScore < 100))
             System.out.println("Greater than second top score and less then 100");
+
+        if ((topScore > 90) || (secondTopScore <= 90))
+            System.out.println("One of these tests is true");
+
+        int newValue = 50;
+        if(newValue == 50)
+            System.out.println("This is an error");
+
+        boolean isCar = false;
+        if (isCar == true)
+            System.out.println("This is not supposed to happen");
+
+        isCar = true;
+        boolean wasCar = isCar ? true : false;
+        if (wasCar)
+        System.out.println("wasCar is True");
+
+        // 1. Create a double variable with the value 20
+        // 2. Create a second variable of type double with te value 80
+        // 3. add both numbers up and multiply by 25
+        // 4. Use the remainder operator to figure out the remainder from the sum of #3 divided by 40
+        // 5. write an "if" statement that displays a message "Total was over the limit
+        //    if the remaining total (#4) is equal to 20 or less.
+
+        double firstValue = 20;
+        double secondValue = 80;
+        double outcome = (firstValue + secondValue) * 25;
+        System.out.println("outcome equals " + outcome);
+        double remainder = outcome % 40;
+        System.out.println("remainder equals " + remainder);
+
+        if (remainder <= 20)
+            System.out.println("Total was over the limit with " + remainder);
+
+
+
 
 
     }
