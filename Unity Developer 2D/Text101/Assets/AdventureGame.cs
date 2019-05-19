703a57a797ba3291f701324629c125ac622efffd
Buddy Lodder <b.lodder@bl-ict.nl>
2019-05-19 23:57:45 +0200

    added 2 room, logic for the

--- Unity Developer 2D/Text101/Assets/AdventureGame.cs
+++ Unity Developer 2D/Text101/Assets/AdventureGame.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,7 +11,6 @@ public class AdventureGame : MonoBehaviour
 
     State state;
 
-
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +21,25 @@ public class AdventureGame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStates[0];
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha2 ))
+        {
+            state = nextStates[1];
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            state = nextStates[2];
+        }
+
+        textComponent.text = state.GetStateStory();
     }
 }
--- Unity Developer 2D/Text101/Assets/State.cs
+++ Unity Developer 2D/Text101/Assets/State.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "State")]
 public class State : ScriptableObject
 {
-   [TextArea(14,10)] [SerializeField] string storyText;
+    [TextArea(14,10)] [SerializeField] string storyText;
+    [SerializeField] State[] nextStates;
 
     public string GetStateStory()
     {
         return storyText;
     }
+
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+
 }
