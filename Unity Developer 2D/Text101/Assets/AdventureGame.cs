7ef284a52537c91da077c8d80c89fea0372e3863
Buddy Lodder <b.lodder@bl-ict.nl>
2019-05-19 22:26:41 +0200

    Added scriptable object

--- Unity Developer 2D/Text101/Assets/AdventureGame.cs
+++ Unity Developer 2D/Text101/Assets/AdventureGame.cs
@@ -6,11 +6,16 @@ using UnityEngine.UI;
 public class AdventureGame : MonoBehaviour
 {
     [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State state;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        textComponent.text = ("Hello Buddy");
+        state = startingState;
+        textComponent.text = state.GetStateStory();
     }
 
     // Update is called once per frame
