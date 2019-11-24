ddcc192fac0a278ba99e5ddeda31d9a50403d61b
Buddy Lodder <b.lodder@bl-ict.nl>
2019-11-24 21:51:40 +0100

    Added primitives and arithmatics to BasicsDemo

--- Java In-Depth/src/com/semanticsquare/basics/BasicsDemo.java
+++ Java In-Depth/src/com/semanticsquare/basics/BasicsDemo.java
@@ -25,16 +25,30 @@ class BasicsDemo {
         System.out.print("world!!");
     }
 
+    static void primitives() {
+        System.out.println("\n\nInside primitives ...");
+        int intHex = 0x0041;
+        System.out.println("intHex: " + intHex);
+        int intBinary = 0b01000001;
+        System.out.println("intBinary: " + intBinary);
+        int intUnderscore = 1_23_456;
+        System.out.println("intUnderscore: " + intUnderscore);
+    }
+
     public static void main (String[] args) {
+        
         print();
 
         int i = 6;
         int j = 2;
+        System.out.println("\n\nInside arithmatics ...");
         System.out.println(i + j);
         System.out.println(i - j);
         System.out.println(i * j);
         System.out.println(i / j);
         System.out.println(i % j);
+        
+        primitives();
     }
 
     // 4. Comments or Disabling code
